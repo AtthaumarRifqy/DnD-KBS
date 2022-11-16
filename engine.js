030515eff53137f3e56086178ced2d2371628263
AtthaumarRifqy <matthaumar@gmail.com>
2022-11-16 11:09:48 +0700

    added keywords to rules and engine

--- engine.js
+++ engine.js
@@ -3,6 +3,10 @@ let rules = require('./rules/dnd-kbs-rules.json');
 
 const processEngine = (inputs, decisions) => {
   let engine = new Engine(decisions);
+  engine.addOperator('isKeywordsEmpty', (fact, value) => {
+    if (!fact.length) return value;
+    return !value;
+  })
 
   return engine.run(inputs).then((results) => {
     console.log(results.events);
@@ -10,15 +14,15 @@ const processEngine = (inputs, decisions) => {
   });
 };
 
-// Creating input parameter
 const inputs = {
-  'player-level': 3,
+  'min-player-level': 3,
+  'max-player-level': 8,
   'adventure-type': 'Standard',
   'dm-difficulty': 'Easy',
   'player-difficulty': 'Easy',
-  location: 'Rural Land',
-  'published-year': 2021,
+  'location': 'Rural Land',
+  'published-year': 2022,
+  'keywords': ['Grim & Gritty', 'Combat']
 };
 
-// Pass the decisions property from employeesalary rule object
 processEngine(inputs, rules.decisions);
